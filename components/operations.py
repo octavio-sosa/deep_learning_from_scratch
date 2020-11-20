74e1c13da0e788040610c7990e4465de30f28335
octavio-sosa <octavio@ku.edu>
2020-11-19 21:02:47 -0600

    define base operations

--- components/operations.py
+++ components/operations.py
@@ -1,22 +1,72 @@
+import numpy as np
+
 class Operation():
     '''
     Base operation
     '''
-
     def __init__(self):
         pass
 
-    def forward(self, input_: np.ndarray):
-    '''
-    '''
-    self.input_ = input_
-    self.output = self.f()
 
-    return self.output
+    def forward(self, input_: np.ndarray):
+        '''
+        '''
+        self.input_ = input_
+        self.output = self.f()
+        return self.output
+
 
     def backward(self, output_grad: np.ndarray) -> np.ndarray:
-    assert_same_shape(self.output, output_grad)
-
-
-
+        assert_same_shape(self.output, output_grad)
+        self.input_grad = self.get_input_grad(output_grad) 
+        assert_same_shape(self.input_, self.input_grad)
+        return self.input_grad
     
+    
+    def f(self) -> np.ndarray:
+        '''
+        Interface for computing forward func
+        '''
+        raise NotImplementedError()
+
+    def get_input_grad(self, output_grad: np.ndarray) -> np.ndarray:
+        '''
+        Interface for computing partial derivative w.r.t. input
+        '''
+        raise NotImplementedError()
+
+class ParamOperation(Operation):
+    '''
+    Operation with model params as argument
+    '''
+
+    def __init__(self, param: np.ndarray):
+        super().__init__() #inherit from super-class (Operation)
+        self.param = param
+
+    def backward(self, output_grad: np.ndarray) -> np.ndarray:
+        assert_same_shape(self.output, output_grad)
+
+        self.input_grad = self.get_input_grad(output_grad)
+        self.param_grad = self.get_param_grad(output_grad)
+
+        assert_same_shape(self.input_, self.input_grad)
+        assert_same_shape(self.param, self.param_grad)
+
+        return self.input_grad
+
+    def get_param_grad(self, output_grad: np.ndarray) -> np.ndarray:
+        '''
+        Interface for computing partial derivatives w.r.t. params
+        '''
+        raise NotImplementedError()
+
+class WeightTransform(ParamOperation):
+    '''
+    Transformation of inputs using weights
+    '''
+
+    def __init__(self, W: np.ndarray):
+        super().__init__(W)
+
+
