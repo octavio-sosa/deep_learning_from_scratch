c54fdb4d1cfc6708fa6b6b9befa168ce8ec74801
octavio-sosa <octavio@ku.edu>
2020-11-20 00:57:13 -0600

    add forward(...) for Loss

--- components/loss.py
+++ components/loss.py
@@ -1,5 +1,16 @@
+from assert import assert_same_shape
 class Loss():
     def __init__(self):
         pass
+
+    def forward(self, prediction: np.ndarray, target: np.ndarray) -> float:
+        assert_same_shape(prediction, target)
+
+        self.prediction = prediction
+        self.target = target
+
+        loss_val = self._f()
+        return loss_val
+
 
 
