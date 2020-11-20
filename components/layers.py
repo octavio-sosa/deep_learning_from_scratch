32126859431123e6c403afb46c3ad339cb6d94a4
octavio-sosa <octavio@ku.edu>
2020-11-19 22:31:03 -0600

    add description for Layer(...)

--- components/layers.py
+++ components/layers.py
@@ -2,6 +2,9 @@ import numpy as np
 from typing import List
 
 class Layer():
+    '''
+    Base layer
+    '''
     def __init__(self, n_neurons: int):
     self.n_neurons = n_neurons
     self.first = True
