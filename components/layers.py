42c2ace9367cfe40e954b2e232f319bae66d8aa2
octavio-sosa <octavio@ku.edu>
2020-11-19 22:30:19 -0600

    def __init__(...)

---
+++ components/layers.py
@@ -0,0 +1,10 @@
+import numpy as np
+from typing import List
+
+class Layer():
+    def __init__(self, n_neurons: int):
+    self.n_neurons = n_neurons
+    self.first = True
+    self.params: List[np.ndarray] = []
+    self.param_grads: List[np.ndarray] = []
+    self.operations: List[Operation] = []
