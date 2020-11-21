454fa8a6fc969e245e4e8f6c6cc9459f78054185
octavio-sosa <octavio@ku.edu>
2020-11-20 18:09:16 -0600

    add SGD(Optimizer)

--- components/optimizer.py
+++ components/optimizer.py
@@ -11,3 +11,18 @@ class Optimizer():
         '''
         raise NotImplementedError()
 
+class SGD(Optimizer):
+    '''
+    Stochastic Gradient Descent
+    '''
+    def __init__(self, learning_rate: float = 0.01):
+        super().__init__(learning_rate)
+
+    def update_params(self):
+    '''
+    Network params are updated directly (yielded by reference)
+    Self.net attribute is set in Trainer init
+    '''
+        for (param, param_grad) in zip(self.net.yield_params(),
+                                       self.net.yield_param_grads()):
+            param -= self.learning_rate*param_grad
